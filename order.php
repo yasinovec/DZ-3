cb5405217b2219a82e2e4e987cd2df48f46613cf
yasinovec <67464071+yasinovec@users.noreply.github.com>
2021-04-29 14:09:03 +0200

    Update order.php

--- order.php
+++ order.php
@@ -1,5 +1,34 @@
 <?php
 
+//DZ#3.1
+
+$names = ['Anna', 'Boris', 'Max', 'Olga', 'Alex'];
+$users = [];
+
+
+for ($i = 0; $i <= 50; $i++) {
+    $name = $names[array_rand($names, 1)];
+    $user = [
+        'id' => $i,
+        'name' => $name,
+        'age' => mt_rand(18, 45)
+    ];
+    $users[] = $user;
+}
+
+foreach ($users as $user) {
+    $usNames[] = $user['name'];
+    $usAges[] = $user['age'];
+}
+echo '<pre>';
+print_r(array_count_values($usNames));
+print_r(array_sum($usAges)/count($usAges));
+$json = json_encode($users);
+file_put_contents('src/users.json', $json);
+$php = json_decode(file_get_contents('src/users.json'), true);
+
+//DZ#3.2
+
 if (isset($_GET['email'])) {
     $email = $_GET['email'];
     $name = $_GET["name"];
@@ -40,10 +69,3 @@ if ($email==$res['email']) {
     echo 'Номер вашего заказа: ' . $id['MAX(`no`)'];
     $connect->close();
 }
-
-//$connect = mysqli_connect("localhost", "root", "root", "orders");
-//$query = "INSERT INTO orders (`name`, phone, email, street, home, part, appt, floor) VALUES
-//('$name','$phone','$email','$street','$home','$part','$appt','$floor')";
-//mysqli_query($connect, $query);
-//$connect->close();
-//header('location: index.html');
